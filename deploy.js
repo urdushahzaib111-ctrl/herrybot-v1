153eb259ccc748cb6a649819fa4f0c9b69b73db6
urdushahzaib111-ctrl <urdushahzaib111@gmail.com>
2026-07-27 06:36:27 +0500

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -24,11 +24,12 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 (async () => {
     try {
         console.log('Refreshing 40 application commands...');
-
+      
         const data = await rest.put(
-            Routes.applicationCommands(process.env.CLIENT_ID),
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
             { body: commands.map(command => command.toJSON()) },
         );
+        
 
         console.log(`Successfully reloaded ${data.length} application commands!`);
     } catch (error) {
